e7fa5fbb1e3635d6a7517fdfe9bb4437abc365f6
kermara <35270616+kermara@users.noreply.github.com>
2022-03-21 22:58:59 +0200

    kierroksset lisätty

--- kneeangle/src/components/Datatable.js
+++ kneeangle/src/components/Datatable.js
@@ -1,47 +1,55 @@
 import { useState } from 'react'
 
+
 let arrayx = {0: 0}
+
 const Datatable = ({ data }) => {
 
    if (data !== null) {
     arrayx = data
   }
 
-  let xvalues = []
-  console.log('eka',arrayx.length)
+  
+  console.log('datan koko',arrayx.length)
   console.log('datatablen arrayx', arrayx)
-  console.log('datatablen eka 0 ja 0 arvo', arrayx[0].data[0].x)
   console.log('datatablen counter', arrayx[arrayx.length-1].counter)
   console.log('angle', arrayx[0].angle)
-  
+  console.log('leg', arrayx[0].leg)
 
+  let angleValues = []
  
   for(let i = 0; i < arrayx.length; i++){
-      for(let j = 0; j < 4; j++){
-        //  xvalues.push(arrayx[i].data[2].x)
-        xvalues.push(180-arrayx[0].angle)
-        }   
+         //  xvalues.push(arrayx[i].data[2].x)        
+        angleValues.push(180-arrayx[i].angle)
       }
   
   
-   console.log('xvalues', xvalues)
+   console.log('angelvalues', angleValues)
 
-  console.log(xvalues[0], xvalues.length)
-  const minX = Math.min(xvalues[0], xvalues.length)
-  console.log(minX)
-  const maxX = Math.max(xvalues[0], xvalues.length)
-  console.log(maxX)
-  const meanX = xvalues.reduce((a, b) => a + b, 0) / xvalues.length
-  console.log(meanX)
-  xvalues = xvalues.map((k) => {
+  console.log(angleValues[0], angleValues.length)
+  const minX = Math.min.apply(Math, angleValues).toFixed(2)
+  console.log('min', minX)
+  const maxX = Math.max.apply(Math, angleValues).toFixed(2)
+  console.log('max', maxX)
+  const meanX = (angleValues.reduce((a, b) => a + b, 0) / angleValues.length).toFixed(2)
+  console.log('mean', meanX)
+  angleValues = angleValues.map((k) => {
     return (k - meanX) ** 2
   })
-  let sum = xvalues.reduce((a, b) => a + b, 0)
-  let variance = sum / xvalues.length
-  const stdX = Math.sqrt(variance)
-  console.log(stdX)
+  let sum = angleValues.reduce((a, b) => a + b, 0)
+  let variance = sum / angleValues.length
+  const stdX = Math.sqrt( variance).toFixed(2)
+  console.log('std', stdX)
 
   const tabledata = [
+    {
+      name:'Leg',
+      value: arrayx[0].leg
+    },
+    {
+      name: 'Rounds',
+      value: arrayx[arrayx.length-1].counter
+    },
     {
       name: 'Minimum',
       value: minX
@@ -57,15 +65,13 @@ const Datatable = ({ data }) => {
     {
       name: 'Standard deviation',
       value: stdX
-    }
+    },
   ]
-
   
   const [state, setState] = useState(tabledata)
 
-
   return (
-    <table>
+      <table>
       <tr key={"header"}>
       </tr>
       {state.map((item) => (
--- kneeangle/src/components/Stats.js
+++ kneeangle/src/components/Stats.js
@@ -1,6 +1,6 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-let dataset = { name: '1', x: 0.37032198905944824, y: 0.5510018467903137, z: 0.00781952962 }
+let dataset = { counter: '1', x: 0.37032198905944824, y: 0.5510018467903137, z: 0.00781952962 }
 
 const Stats = ({ data }) => {
 
@@ -10,34 +10,39 @@ const Stats = ({ data }) => {
      }
 
      console.log('stats', dataset)
+
+     
+    
+
+ 
      
      return (
           <div>
           <LineChart width={320} height={180} data={dataset}
                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-               <XAxis dataKey="name" />
+               <XAxis dataKey="counter" />
                <YAxis />
                <CartesianGrid strokeDasharray="3 3" />
                <Tooltip />
                <Legend verticalAlign="top" height={36}/>
-               <Line name="hip x" type="monotone" dataKey="data.0.x" stroke="#CDB4D8" activeDot={{ r: 8 }} />
-               <Line name="hip y" type="monotone" dataKey="data.0.y" stroke="#FFAFCC" />
-               <Line name="hip z" type="monotone" dataKey="data.0.z" stroke="#BDE0FE" />
+               <Line name="hip 0 x" type="monotone" dataKey="data.1.x" stroke="#CDB4D8" activeDot={{ r: 8 }} />
+               <Line name="hip 0 y" type="monotone" dataKey="data.1.y" stroke="#FFAFCC" />
+               <Line name="hip  0 z" type="monotone" dataKey="data.1.z" stroke="#BDE0FE" />
           </LineChart>
           <LineChart width={320} height={180} data={dataset}
                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-               <XAxis dataKey="name" />
+               <XAxis dataKey="counter" />
                <YAxis />
                <CartesianGrid strokeDasharray="3 3" />
                <Tooltip />
                <Legend verticalAlign="top" height={36}/>
-               <Line name="hip x" type="monotone" dataKey="data.1.x" stroke="#CDB4D8" activeDot={{ r: 8 }} />
-               <Line name="hip y" type="monotone" dataKey="data.1.y" stroke="#FFAFCC" />
-               <Line name="hip z" type="monotone" dataKey="data.1.z" stroke="#BDE0FE" />
+               <Line name="hip 1 x" type="monotone" dataKey="data.1.x" stroke="#CDB4D8" activeDot={{ r: 8 }} />
+               <Line name="hip 1 y" type="monotone" dataKey="data.1.y" stroke="#FFAFCC" />
+               <Line name="hip 1 z" type="monotone" dataKey="data.1.z" stroke="#BDE0FE" />
           </LineChart>
           <LineChart width={320} height={180} data={dataset}
                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-               <XAxis dataKey="name" />
+               <XAxis dataKey="counter" />
                <YAxis />
                <CartesianGrid strokeDasharray="3 3" />
                <Tooltip />
@@ -48,14 +53,25 @@ const Stats = ({ data }) => {
           </LineChart>
           <LineChart width={320} height={180} data={dataset}
                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-               <XAxis dataKey="name" />
+               <XAxis dataKey="counter" />
                <YAxis />
                <CartesianGrid strokeDasharray="3 3" />
                <Tooltip />
                <Legend verticalAlign="top" height={36}/>
-               <Line name="ancle x" type="monotone" dataKey="data.3.x" stroke="#CDB4D8" activeDot={{ r: 8 }}/>
-               <Line name="ancle y" type="monotone" dataKey="data.3.y" stroke="#FFAFCC" />
-               <Line name="ancle z" type="monotone" dataKey="data.3.z" stroke="#BDE0FE" />
+               <Line name="angle x" type="monotone" dataKey="data.3.x" stroke="#CDB4D8" activeDot={{ r: 8 }}/>
+               <Line name="angle y" type="monotone" dataKey="data.3.y" stroke="#FFAFCC" />
+               <Line name="angle z" type="monotone" dataKey="data.3.z" stroke="#BDE0FE" />
+          </LineChart>
+           <LineChart width={640} height={360} data={dataset}
+               margin={{ top: 5, right: 20, left: 20, bottom: 10 }}>
+               <XAxis dataKey="counter" />
+               <YAxis />
+               <CartesianGrid strokeDasharray="3 3" />
+               <Tooltip />
+               <Legend verticalAlign="top" height={50}/>
+               <Line name="knee angle" type="monotone" dataKey="angle" stroke="#E040FB" activeDot={{ r: 8 }}/>
+               {/* <Line name="ancle y" type="monotone" dataKey="data.3.y" stroke="#FFAFCC" />
+               <Line name="ancle z" type="monotone" dataKey="data.3.z" stroke="#BDE0FE" /> */}
           </LineChart>
           
          </div>
