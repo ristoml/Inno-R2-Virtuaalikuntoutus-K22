c65053795a0e8f6b690c84f9288c7f31dba1054a
samuelahw <samuel.ahjoniemi@gmail.com>
2022-04-07 16:30:48 +0300

    useeffect

--- kneeangle/src/components/Datapanel.js
+++ kneeangle/src/components/Datapanel.js
@@ -3,45 +3,57 @@ import Stats from "./Stats";
 import Datatable from "./Datatable";
 import Playback from './Playback';
 import axios from 'axios';
+import { useEffect, useState } from 'react'
 
-let data = []
 
 const Datapanel = ({ onClick, squatData }) => {
+
+    const [data, setCurrentdata] = useState(null)
+
+    useEffect(() => {
+        //viimeisin haku
+        const promise = axios.get('http://localhost:3001/api/getlatest')
+        promise.then(response => {
+            setCurrentdata(response.data.data)
+        })
+    }, [squatData])
+
+
     if (squatData !== null) {
         //lisääminen tietokantaan
-        const resultObject = {
-            date: new Date().toISOString(),
-            data: squatData
-        }
-        axios
-            .post('http://localhost:3001/api/addResult', resultObject)
-            .then(response => {
-                console.log(response)
-            }).catch(error => { console.error(error.response.data) })
-
+        /* const resultObject = {
+             date: new Date().toISOString(),
+             data: squatData
+         }
+         axios
+             .post('http://localhost:3001/api/addResult', resultObject)
+             .then(response => {
+                 console.log(response)
+             }).catch(error => { console.error(error.response.data) })
+             */
     }
-    //viimeisimmän haku tietokannasta
-    const promise = axios.get('http://localhost:3001/api/results/latest')
-    promise.then(response => {
-        data = response.data.data
-    })
-    setTimeout(function () {
-        data = {}
-      }, 30000)
+
+
 
 
     return (
-        <div className='data-panel'>
-            <Stats data={data} />
-            <Datatable data={data} />
-            <Button
-                color={'gray'}
-                onClick={onClick}
-                text={'Close'}
-            />
-        </div>
+        <>
+            {data &&
+                <div className='data-panel'>
+                    <Stats data={data} />
+                    <Datatable data={data} />
+                    <Button
+                        color={'gray'}
+                        onClick={onClick}
+                        text={'Close'}
+                    />
+                </div>
+            }
+        </>
 
     )
 }
+
+
 
 export default Datapanel
