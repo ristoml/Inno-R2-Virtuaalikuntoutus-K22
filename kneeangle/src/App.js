f37dae08e656bb974c2ba6c8a2b3f8ae4fbf3c64
ristoml <ristoml@metropolia.fi>
2022-04-12 00:54:41 +0300

    delete unnecessary files

--- kneeangle/src/App.js
+++ kneeangle/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Info from './pages/Info'
 
-const App = () => {
+const App = () => {  
   return (
     <Router>
       <div className='container'>
