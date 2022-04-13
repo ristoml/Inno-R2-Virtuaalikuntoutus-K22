3b4b125c8abd206636b6b40782deccb15145660b
tapsaca <98756983+tapsaca@users.noreply.github.com>
2022-04-13 15:07:33 +0300

    Change modelComplexity and fix linechart

--- frontend/src/components/results/Stats.js
+++ frontend/src/components/results/Stats.js
@@ -1,67 +1,53 @@
-import { Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, AreaChart } from 'recharts';
-import { useEffect, useState } from "react";
+import { XAxis, YAxis, CartesianGrid, Tooltip, Legend, LineChart, Line, ReferenceLine } from 'recharts';
+import { useEffect, useState } from 'react';
 
 let dataset = { counter: '1', x: 0.37032198905944824, y: 0.5510018467903137, z: 0.00781952962 }
 
-
 const Stats = ({ data }) => {
-     const [sdata, setData] = useState(data);
+  const [sdata, setData] = useState(data);
+  useEffect(() => {
+    setData(data);
+  }, [data]);
 
-     useEffect( () => {
-         setData(data);
-     }, [data]); 
+  if (sdata !== null) {
+    dataset = sdata
+  }
 
-     if (sdata !== null) {
-          dataset = sdata
-     }
+  console.log('stats', data)
 
-     console.log('stats', dataset)
-
-     const gradientOffset = () => {
-          const dataMax = Math.max(...dataset.map((i) => i.angle));
-          const dataMin = Math.min(...dataset.map((i) => i.angle));
-
-          if (dataMax <= 0) {
-               return 0
-          }
-          else if (dataMin >= 0) {
-               return 1
-          }
-          else {
-               return dataMax / (dataMax - dataMin);
-          }
-     }
-
-
-     const off = gradientOffset();
-
-
-     return (
-          <div>
-
-               <AreaChart width={640} height={360} data={dataset}
-                    margin={{ top: 5, right: 20, left: 20, bottom: 10 }}>
-                    <XAxis dataKey="counter" />
-                    <YAxis />
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <Tooltip
-                         formatter={(value) => value.toFixed(2)}
-                         wrapperStyle={{ top: -120, left: 150 }}
-                    />
-                    <defs>
-                         <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                              <stop offset={off} stopColor="green" stopOpacity={1} />
-                              <stop offset={off} stopColor="red" stopOpacity={1} />
-                         </linearGradient>
-                    </defs>
-                    <Legend verticalAlign="top" height={50} />
-                    <Area name="Knee angle" type="monotone" dataKey="angle" stroke="#7700bd" activeDot={{ r: 8 }} />
-
-               </AreaChart>
-
-          </div>
-
-     );
+  return (
+    <div>
+      <LineChart width={600} height={700} data={dataset}
+        margin={{ top: 5, right: 20, left: 20, bottom: 10 }}>
+        <XAxis dataKey='counter' />
+        <YAxis domain={[-50, 50]} allowDataOverflow={true} ticks={[-50, -45, -40, -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]} />
+        <CartesianGrid strokeDasharray='3 3' />
+        <Tooltip
+          formatter={(value) => value.toFixed(2)}
+          wrapperStyle={{ top: -120, left: 150 }}
+        />
+        <ReferenceLine
+          stroke='red'
+          strokeWidth={2}
+          y={-10}
+        />
+        <ReferenceLine
+          stroke='red'
+          strokeWidth={2}
+          y={10}
+        />
+        <Legend verticalAlign='top' height={50} />
+        <Line
+          name='knee angle'
+          type='monotone'
+          dataKey='angle'
+          dot={false}
+          stroke='#a340d9'
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </div>
+  )
 }
 
 export default Stats;
